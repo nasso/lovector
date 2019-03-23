a19fbbcba60fea80499f60913ee3fe3e7fa52fcd
nasso <nassomails@gmail.com>
2019-03-23 21:38:32 +0100

    Update HSLA to RGBA function code to follow guidelines

--- lovector/svg/common.lua
+++ lovector/svg/common.lua
@@ -192,8 +192,29 @@ function common.get_attr(element, attrname, default)
     return element:get_attribute(attrname, INHERIT[attrname], default)
 end
 
+function common.hsla_to_rgba(h, s, l, a)
+    if s <= 0 then
+        return l, l, l, a
+    end
+
+    h, s, l = h * 6, s, l
+    local c = (1 - math.abs(2 * l - 1)) * s
+    local x = (1 - math.abs(h % 2 - 1)) * c
+    local m, r, g, b = (l - 0.5 * c), 0, 0, 0
+
+    if     h < 1 then r, g, b = c, x, 0
+    elseif h < 2 then r, g, b = x, c, 0
+    elseif h < 3 then r, g, b = 0, c, x
+    elseif h < 4 then r, g, b = 0, x, c
+    elseif h < 5 then r, g, b = x, 0, c
+    else              r, g, b = c, 0, x
+    end
+
+    return (r + m), (g + m), (b + m), a
+end
+
 -- parse a color definition, returning the RGBA components in the 0..1 range
-function common.colorparse(str, default_r, default_g, default_b, default_a)
+function common.color_parse(str, default_r, default_g, default_b, default_a)
     if str == nil then
             return default_r, default_g, default_b, default_a
         end
@@ -398,8 +419,8 @@ function common.gen_subpath(svg, element, vertices, closed, options)
     -- attributes!
 
     --  colors (red/green/blue)
-    local f_red, f_green, f_blue, f_alpha = common.colorparse(common.get_attr(element, "fill", "black"))
-    local s_red, s_green, s_blue, s_alpha = common.colorparse(common.get_attr(element, "stroke", "none"))
+    local f_red, f_green, f_blue, f_alpha = common.color_parse(common.get_attr(element, "fill", "black"))
+    local s_red, s_green, s_blue, s_alpha = common.color_parse(common.get_attr(element, "stroke", "none"))
 
     -- opacity
     local opacity = tonumber(common.get_attr(element, "opacity", "1"), 10)
@@ -459,7 +480,7 @@ function common.gen_subpath(svg, element, vertices, closed, options)
     end
 
     if options["path_debug"] then
-        local r,g,b = common.HSL(math.random(), 1, .5)
+        local r,g,b = common.hsla_to_rgba(math.random(), 1, .5)
         result = result .. "love.graphics.setColor(" .. r .. ", " .. g .. ", " .. b .. ", 0.5)\n"
         result = result .. "love.graphics.setPointSize(5)\n"
         result = result .. "love.graphics.points(" .. bufferid .. ")\n"
@@ -478,21 +499,6 @@ function common.gen_path(svg, element, path, options)
     end
 
     return result
-end
-
-function common.HSL(h, s, l, a)
-    if s<=0 then return l,l,l,a end
-    h, s, l = h*6, s, l
-    local c = (1-math.abs(2*l-1))*s
-    local x = (1-math.abs(h%2-1))*c
-    local m,r,g,b = (l-.5*c), 0,0,0
-    if h < 1     then r,g,b = c,x,0
-    elseif h < 2 then r,g,b = x,c,0
-    elseif h < 3 then r,g,b = 0,c,x
-    elseif h < 4 then r,g,b = 0,x,c
-    elseif h < 5 then r,g,b = x,0,c
-    else              r,g,b = c,0,x
-    end return (r+m),(g+m),(b+m),a
 end
 
 function common.gen(svg, element, options)
